736553a63b4ac005590e443ae153a8c0ea848625
Irham Rahmat Saleh <irhamrahmatsaleh1997@gmail.com>
2025-01-01 15:40:25 +0700

    modified lesson

--- scripts/12-rock-paper-scissors.js
+++ scripts/12-rock-paper-scissors.js
@@ -18,9 +18,13 @@ if (!score) {
 
 let isAutoPlaying = false;
 let intervalId;
+
+//const autoPlay = () => {
+
+//}
 function autoPlay() {
   if (!isAutoPlaying) {
-    intervalId = setInterval(function () {
+    intervalId = setInterval(() => {
       const playerMove = pickComputerMove();
       playGame(playerMove);
     }, 1000);
@@ -30,6 +34,42 @@ function autoPlay() {
     isAutoPlaying = false;
   }
 }
+
+document.querySelector('.js-rock-button').addEventListener('click', () => {
+  playGame('rock');
+});
+
+document.querySelector('.js-paper-button').addEventListener('click', () => {
+  playGame('paper');
+});
+
+document.querySelector('.js-scissors-button').addEventListener('click', () => {
+  playGame('scissors');
+});
+
+// document.body.addEventListener('keydown', (event) => {
+//   if (event.key === 'r') {
+//     playGame('rock');
+//   } else if (event.key === 'p') {
+//     playGame('paper');
+//   } else if (event.key === 's') {
+//     playGame('scissors');
+//   }
+// })
+document.body.addEventListener('keydown', (event) => {
+  if (event.key === 'a') {
+    autoPlay();
+  } else if (event.key === 'r') {
+    playGame('rock');
+  } else if (event.key === 'p') {
+    playGame('paper');
+  } else if (event.key === 's') {
+    playGame('scissors');
+  } else if (event.key === 'q') {
+    clearInterval(intervalId);
+    isAutoPlaying = false;
+  }
+})
 function playGame(playerMove) {
   const computerMove = pickComputerMove();
   let result = '';
